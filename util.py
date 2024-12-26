d08628c861830bd90316224b60cb38786ff3533c
Yazh-car <yazhcar20@gmail.com>
2024-12-26 15:01:16 +0100

    Nouveau commentaire
    bhibickds

--- util.py
+++ util.py
@@ -7,3 +7,6 @@ def load_cross_section(filename):
     """Takes a text file with two columns of numbers and return two arrays : one for each column"""
     data_cs = load_csv(filename)
     return data_cs[:,0], data_cs[:,1]
+
+
+#jbhvihsbvjhbsdhjvbds
