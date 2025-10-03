a128722f875ee3281f4cf93ae0bef75b1af28626
Charlélie CARER <charlelie.carer@gmail.com>
2025-10-03 18:22:53 +0200

    Created exp for Thorsteinsonn reproduction

---
+++ src/experiments/E08_N2_Thorsteinsonn/config.py
@@ -0,0 +1,20 @@
+from scipy.constants import e, k, pi
+import numpy as np
+
+
+
+config_dict = {
+
+        # Geometry
+        'R': 10e-2,
+        'L': 10e-2,
+        
+        # Neutral flow
+        'target_pressure': 1.33, #in Pa
+
+        # Electrical
+        'omega': 13.56e6 * 2 * pi,
+        'N': 3,
+        'R_coil': 2
+
+}
