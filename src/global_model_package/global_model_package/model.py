1b6270a2702b8424a8943ad399260db20f50a28c
Charlélie CARER <charlelie.carer@gmail.com>
2025-10-17 22:05:50 +0200

    Increased tolerance in model solve_ivp

--- src/global_model_package/global_model_package/model.py
+++ src/global_model_package/global_model_package/model.py
@@ -236,7 +236,7 @@ class GlobalModel:
         """
         #y0 = np.array([self.chamber.n_e_0, self.chamber.n_g_0, 0, self.chamber.T_e_0, self.chamber.T_g_0, 0])
         y0 = np.array(initial_state)  #np.array([self.chamber.n_e_0, self.chamber.n_g_0, 0, self.chamber.T_e_0, self.chamber.T_g_0, 0])
-        sol = solve_ivp(self.f_dy, (t0, tf), y0, method='LSODA', rtol=1e-1, atol=1e-3, first_step=5e-12, min_step=1e-12, args=args, events=stop_event)    # , max_step=1e-7
+        sol = solve_ivp(self.f_dy, (t0, tf), y0, method='LSODA', rtol=1e-3, atol=1e-3, first_step=5e-12, min_step=1e-12, args=args, events=stop_event)    # , max_step=1e-7
         #log_file_path=self.simulation_name
         self.var_tracker.save_tracked_variables()
         print("Variables saved")
@@ -325,6 +325,7 @@ class GlobalModel:
     
 def stop_event(t, state):
     if t > 1.1e-2 and state[0] < 1e10:
+        print("Early stop event triggered because of electron density below 1e10 m^-3 for t > 1.1e-2 s")
         return 0.0
     return 1.0  # positive otherwise
 
